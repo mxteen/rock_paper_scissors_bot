d8d34cfb6f3b1b06563cf8e4553a066b695b0d1a
Maksim Terentev <maxim.i.terentev@gmail.com>
2024-06-10 19:05:44 +0300

    added handlers

---
+++ handlers/user_handlers.py
@@ -0,0 +1,15 @@
+from aiogram.types import Message
+from aiogram.filters import Command, CommandStart
+from lexicon.lexicon import LEXICON_RU
+
+
+# Этот хэндлер срабатывает на команду /start
+@dp.message(CommandStart())
+async def process_start_command(message: Message):
+    await message.answer(text=LEXICON_RU['/start'])
+
+
+# Этот хэндлер срабатывает на команду /help
+@dp.message(Command(commands='help'))
+async def process_help_command(message: Message):
+    await message.answer(text=LEXICON_RU['/help'])
